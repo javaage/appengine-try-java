afe91c7d0b305c07c046e3cb1a0537059ad96c67
javaage <subing.xiao@pactera.com>
2017-12-26 17:25:44 +0800

    test

--- src/main/java/myapp/DemoServlet.java
+++ src/main/java/myapp/DemoServlet.java
@@ -28,57 +28,40 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DemoServlet extends HttpServlet {
-  @Override
-  public void doGet(HttpServletRequest req, HttpServletResponse resp)
-      throws IOException {
-	  resp.setContentType("text/plain");
-	    resp.getWriter().println("{ \"name\": \"" + "world" + "\" }");
-//	    return;
-//	  
-//	  	String instanceConnectionName = "newagent-25039:asia-northeast1:la";
-//
-//	    // TODO: fill this in
-//	    // The database from which to list tables.
-//	    String databaseName = "la";
-//
-//	    String username = "postgres";
-//
-//	    // TODO: fill this in
-//	    // This is the password that was set via the Cloud Console or empty if never set
-//	    // (not recommended).
-//	    String password = "la123456";
-//	    
-//	    //[START doc-example]
-//	    String jdbcUrl = String.format(
-//	        "jdbc:postgresql://google/%s?socketFactory=com.google.cloud.sql.postgres.SocketFactory"
-//	            + "&socketFactoryArg=%s",
-//	        databaseName,
-//	        instanceConnectionName);
-//	    
-//	    
-//	 
-//	    Connection connection;
-//		try {
-//			Class.forName("org.postgresql.Driver");
-//			//connection = DriverManager.getConnection(jdbcUrl, username, password);
-//			connection = DriverManager.getConnection("jdbc:postgresql://192.168.120.128:5432/la", "postgres", "19786028");
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		String instanceConnectionName = "newagent-25039:asia-northeast1:la";
+
+		// TODO: fill this in
+		// The database from which to list tables.
+		String databaseName = "la";
+
+		String username = "postgres";
+
+		String password = "la123456";
+
+		String jdbcUrl = String
+				.format("jdbc:postgresql://google/%s?socketFactory=com.google.cloud.sql.postgres.SocketFactory"
+						+ "&socketFactoryArg=%s", databaseName, instanceConnectionName);
+
+		Connection connection;
+		try {
+			Class.forName("org.postgresql.Driver");
+			// connection = DriverManager.getConnection(jdbcUrl, username, password);
+//			connection = DriverManager.getConnection("jdbc:postgresql://192.168.120.128:5432/la", "postgres",
+//					"19786028");
 //			Statement statement = connection.createStatement();
-//			ResultSet resultSet =
-//			          statement.executeQuery(
-//			              "SELECT * FROM LA_USER");
-//			      while (resultSet.next()) {
-//			        System.out.println(resultSet.getString(1) + "." + resultSet.getString(2));
-//			      }
-//		} catch (SQLException | ClassNotFoundException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//			resp.setContentType("text/plain");
-//		    resp.getWriter().println("{ \"name\": \"" + e.getMessage() + "\" }");
-//		}
-//	    //[END doc-example]
-//	  
-//	  
-//	    resp.setContentType("text/plain");
-//	    resp.getWriter().println("{ \"name\": \"" + jdbcUrl + "\" }");
-  }
+//			ResultSet resultSet = statement.executeQuery("SELECT * FROM LA_USER");
+//			while (resultSet.next()) {
+//				System.out.println(resultSet.getString(1) + "." + resultSet.getString(2));
+//			}
+		} catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+			resp.setContentType("text/plain");
+			resp.getWriter().println("{ \"name\": \"" + e.getMessage() + "\" }");
+		}
+
+		resp.setContentType("text/plain");
+		resp.getWriter().println("{ \"name\": \"" + jdbcUrl + "\" }");
+	}
 }
